719d77ae1b24840c2a3124c4aa1ad60af3028eb5
Sally Steuterman <sally.steuterman@gmail.com>
2023-01-20 12:49:03 -0600

    Initial setup with Data Types examples

---
+++ HelloMethods/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloMethods
+{ 
+	public class Message
+	{
+        public static string GetMessage(string lang)
+        {
+            if (lang.Equals("sp"))
+            {
+                return "Hola Mundo";
+            }
+            else if (lang.Equals("fr"))
+            {
+                return "Bonjour le monde";
+            }
+            else
+            {
+                return "Hello World";
+            }
+        }
+    }
+}
+
---
+++ csharp-web-development-examples/Program.cs
@@ -0,0 +1,11 @@
+﻿double fahrenheit;
+double celsius;
+string input;
+
+Console.WriteLine("Temperature in F:");
+input = Console.ReadLine();
+fahrenheit = double.Parse(input);
+
+celsius = (fahrenheit - 32) * 5 / 9;
+Console.WriteLine("The Temperature in C is: " + celsius);
+Console.ReadLine();
