44d984a926dc4aac274c820bd89bccaa0e854dfd
Sally Steuterman <sally.steuterman@gmail.com>
2023-01-22 18:27:06 -0600

    Updating readme and hellomethods

--- HelloMethods/Program.cs
+++ HelloMethods/Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿string message = Message.GetMessage("fr");
+Console.WriteLine(message);
+Console.ReadLine();
